cb7a6798a5e313d008ce11e62295e1e08ef65ba4
Sumin Bae <55087554+SuminBae97@users.noreply.github.com>
2021-10-12 16:40:41 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,157 @@
-from sklearn.cluster import KMeans, DBSCAN
-import numpy as np
-import argparse
-import utils
-import model
 import os
+import cv2
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from pprint import pprint
 
-if __name__ == "__main__":
-    """
-    1.get video frame
-    2.YOLO detection
-    3.Crop person image
-    4.Clustering 
-    """
-    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-    os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+classFile='coco.names'
+classnames=[]
 
-    path = '/path/'
+whT=320
+confidenceThreshold =0.5
+nmsThreshold = 0.3
+#lower it is acc
 
-    ## Get Frame from Video
-    utils.get_frame_from_video(path, '1554.mp4')
-    utils.write_train_list(path)
 
-    ## YOLO & get Coordinate
-    utils.excute_YOLO()
-    coord = utils.get_person_coord(path)
-    utils.crop_person_image(coord, path)
 
-    ## Feature Extraction
-    filenames = os.listdir(os.path.join(path, 'persons'))
-    imgs_dict = utils.get_images(path, filenames, target_size=(299, 299))
+modelconfig = 'yolov3.cfg'
+modelweights = 'yolov3.weights'
 
-    model = model.get_model()
-    img_feature_vector = utils.feature_vectors(imgs_dict, model)
+#yolo model
+net = cv2.dnn.readNetFromDarknet(modelconfig,modelweights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
-    ## croped person image Train
-    # model.fit()
-    images = list(img_feature_vector.values())
-    
-    ## Clustering
-    # kmeans = KMeans(n_clusters=5, init='k-means++')
-    # kmeans.fit(images)
-    # y_kmeans = kmeans.predict(images)
-    dbscan = DBSCAN()
-    y_dbscan = dbscan.fit(images)
 
-    file_names = list(imgs_dict.keys())
-    groups = utils.make_group(file_names, dbscan)
-    # for i in groups:
-    #     print(i)
-    print(groups)
+with open(classFile,'rt') as f:
+    classnames = f.read().rstrip('\n').split('\n')
+print(classnames)
+print(len(classnames))
+
+
+
+def findobject(outputs,img):
+    hT,wT,cT = img.shape
+    bbox = []
+    classIds= []
+    confs=[]
+
+    for output in outputs:
+        for det in output:
+            scores =det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+
+            if confidence>confidenceThreshold:
+                w,h = int(det[2]*wT) , int(det[3]*hT)
+                x,y =  int((det[0]*wT) - w/2) , int((det[1]*hT) - h/2)
+                bbox.append([x,y,w,h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    print(len(bbox))
+    indicies = cv2.dnn.NMSBoxes(bbox,confs,confidenceThreshold,nmsThreshold)
+
+    for i in indicies:
+        i = i[0]
+        box = bbox[i]
+        x,y,w,h = box[0],box[1],box[2],box[3]
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),2)
+
+
+
+        #cv2.putText(img,f'{classnames[classIds[i]].upper()} {int(confs[i]*100)}%',
+        #(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,0,255),1)
+
+    print(x,y,x+w,y+h)
+
+
+def yolo_on_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    count = 0
+
+    while True:
+        success, img = cap.read()
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 200)
+
+        print(success)
+        print(img.shape)
+
+        # input to networ
+        blob = cv2.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+        net.setInput(blob)
+
+        layerNames = net.getLayerNames()
+        # extract only output layer
+
+        outputNames = [layerNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        # print(net.getUnconnectedOutLayers())
+        # print(outputNames)
+
+        outputs = net.forward(outputNames)
+        # print(outputs[0].shape)
+        # print(outputs[1].shape)
+        # print(outputs[2].shape)
+        # print(outputs[0][0])
+
+
+        findobject(outputs, img)
+
+        #확인차 보여줄 때는 imshwo하셈
+        #cv2.imshow("image", img)
+        cv2.imwrite("video/" + f"test{count}.jpg", img)
+        #cv2.waitKey(1)
+
+        # 임시방편용
+        if count == 30:
+            break
+
+        count += 1
+
+
+if __name__=="__main__":
+    cap = cv2.VideoCapture('1554.mp4')
+    count = 0
+
+    while True:
+        success, img = cap.read()
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 200)
+
+        print(success)
+        print(img.shape)
+
+        # input to networ
+        blob = cv2.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+        net.setInput(blob)
+
+        layerNames = net.getLayerNames()
+        # extract only output layer
+
+        outputNames = [layerNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        # print(net.getUnconnectedOutLayers())
+        # print(outputNames)
+
+        outputs = net.forward(outputNames)
+        # print(outputs[0].shape)
+        # print(outputs[1].shape)
+        # print(outputs[2].shape)
+        # print(outputs[0][0])
+
+        findobject(outputs, img)
+
+        cv2.imshow("image", img)
+        cv2.imwrite("video/" + f"test{count}.jpg", img)
+        cv2.waitKey(1)
+
+        # 임시방편용
+        if count == 30:
+            break
+
+        count += 1
+
+
+
+
+
 
