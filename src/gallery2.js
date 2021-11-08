26b16e8a7dc8b770ff9b0479e454d8eeb04186b9
201601972-kimdongwook <62870938+201601972-kimdongwook@users.noreply.github.com>
2021-11-08 21:13:35 +0900

    UI-02-05 gallery2 완성

--- src/gallery2.js
+++ src/gallery2.js
@@ -1,71 +1,30 @@
-const inputVideoBtn = document.getElementById('inputVideoBtn'),
-    video = document.querySelector('.video'),
-    submitVideoBtn = document.getElementById('submitVideoBtn'),
-    goBackBtn = document.getElementById("goBackBtn"),
-    selectedVideos = document.getElementById('selectedVideos');
+const META_FILE_NAME = "meta.txt";
 
+const goBackBtn = document.getElementById("goBackBtn"),
+    frameGallery = document.getElementById("frameGallery");
 
 function goBackHandler() {
     history.back();
 }
 
-function uploadVideo() {
-    window.localStorage.setItem("videoUrl", video.getAttribute("src"));
-}
-
-function submitVideoHandler(event) {
-    event.preventDefault();
-    if (!selectedVideos.childElementCount) {
-        alert("비디오를 선택해 주세요.");
-    } else {
-        // 비디오 업로드
-        // uploadVideo();
-        //location.href = page4_URL;
-        console.log("press upload");
+function composeFrameGallery() {
+    for (let i = 0; i < 5; i++) {
+        const frameName = `result_frame_${i}.jpg`
+        const frameBlockElement = document.createElement('div');
+        frameBlockElement.id = i;
+        frameBlockElement.className = "frame_block";
+        const frame = document.createElement('img');
+        frame.setAttribute('src', "../test_data/target_appear_frame/" + frameName);
+        const frameNameElement = document.createElement('p');
+        frameNameElement.innerText = frameName;
+        frameBlockElement.append(frame, frameNameElement);
+        frameGallery.appendChild(frameBlockElement);
     }
-}
-
-// 비디오는 우선 최대 9개, 변경 필요
-function videoInputHandler() {
-    for (let i = 0; i < 9; i++) {
-        const selectedVideo = inputVideoBtn.files[i];
-        const videoUrl = URL.createObjectURL(selectedVideo);
-
-        const newLi = document.createElement('li');
-        newLi.setAttribute("id", "videoBox");
-        selectedVideos.appendChild(newLi);
-
-
-        const newVideo = document.createElement('video');
-        newVideo.setAttribute("src", videoUrl);
-        newVideo.setAttribute("data-is-play", "paused");
-        newVideo.addEventListener("click", videoClickHandler);
-        newLi.appendChild(newVideo);
-    }
-}
-
-function videoClickHandler() {
-    console.log(video.dataset.isPlay);
-    if (video.dataset.isPlay === "playing") {
-        video.pause();
-        video.dataset.isPlay = "paused";
-    } else {
-        video.play();
-        video.dataset.isPlay = "playing";
-    }
-}
-
-// 페이지2에서 체크한 이미지
-function getCheckedImg() {
-    const checkedImg = document.getElementById("checkedImg");
-    checkedImg.setAttribute("src", window.localStorage.getItem("checkSrc"));
 }
 
 function init() {
-    getCheckedImg();
     goBackBtn.addEventListener('click', goBackHandler);
-    inputVideoBtn.addEventListener("change", videoInputHandler);
-    submitVideoBtn.addEventListener("click", submitVideoHandler);
+    composeFrameGallery();
 }
 
 
